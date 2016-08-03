364ecf292e90021bf3bdeb29e2d11b75a538087b
jtknox91 <jtknox91@gmail.com>
2016-08-03 14:48:52 -0700

    Init index.js to serve static files with placeholder for API routing. Add Procfile for Heroku Local

---
+++ server/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/api', function (req, res) {
+  res.send("API not configured yet, but this route works");
+});
+
+app.use(express.static('client'));
+
+app.listen(5000, function () {
+  console.log("Family Tree Server listening on port 5000!");
+});
