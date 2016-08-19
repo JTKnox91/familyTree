242111a15ddcd8f48ae06228a73a88e766025f1b
jtknox91 <jtknox91@gmail.com>
2016-08-19 14:40:14 -0700

    create db schema if not exists

--- server/db.js
+++ server/db.js
@@ -1,22 +1,48 @@
-if (process.NODE_ENV && process.NODE_ENV !== "development") {
-//production
-var knex = require('knex')({
-  client: 'pg',
-  connection: process.env.DATABASE_URL,
-  searchPath: 'knex,public'
-});
-} else {
-//development
+//Dev implementation
 var knex = require('knex')({
   client: 'sqlite3',
-  connection: {
-    filename: "../db.sqlite"
-  }
+  connection: {filename: "./db.sqlite"},
+  useNullAsDefault: true,
 });
-}
+var bookshelf = require('bookshelf')(knex);
 
-var bookshelf = require('bookshelf')(knex)
+bookshelf.knex.schema.createTableIfNotExists('persons', function (table) {
+  table.increments();
+  table.string('nameFirst');
+  table.string('nameLast');
+  table.string('nameMiddle');
+  table.date('birthday');
+  table.string('photoURL')
+  table.timestamps();
+});
 
-module.exports = bookshelf;    
+bookshelf.knex.schema.createTableIfNotExists('generation', function (table) {
+  table.increments();
+  table.integer('order');
+  table.timestamp();
+});
+
+var Person = bookshelf.Model.extend({
+  tableName: 'persons',
+  spouse: function () {return this.hasOne(Person);},
+  parent: function () {return this.belongsTo(Person);},
+  children: function () {return this.hasMany(Person);}
+});
+var Persons = bookshelf.Collection.extend({
+  model: Person,
+});
+var Generation = bookshelf.Model.extend({
+  tableName: 'generations',
+  persons: function () {return this.hasMany(Person);}
+});
+var Generations = bookshelf.Collection.extend({
+  model: Generation,
+});
 
 
+module.exports = {
+  Person: Person,
+  Persons: Persons,
+  Generation: Generation,
+  Generations: Generations,
+};
--- server/index.js
+++ server/index.js
@@ -10,3 +10,5 @@ app.use(express.static('client'));
 app.listen(process.env.PORT || 5000, function () {
   console.log("Family Tree Server listening on port " + (process.env.PORT || 5000));
 });
+
+var db = require('./db.js');
